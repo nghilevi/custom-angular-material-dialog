d451eaa2dec4ec25efba2c6d788d33b5c2ac6129
Nghi Le <vinhnghi223@gmail.com>
2024-03-08 11:44:51 +0700

    added _themes.scss

--- src/lib/components/dialog/dialog.component.ts
+++ src/lib/components/dialog/dialog.component.ts
@@ -7,5 +7,5 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./dialog.component.scss'],
 })
 export class DialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { title: string }) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { title: string; content: string }) { }
 }
